fa046cf6e6bee677a823cafca0f0c7b365c944c2
Jonathan Di Rico <diricojonathan@gmail.com>
2025-01-30 13:25:58 -0500

    Nightly Update

---
+++ src/config/.feeds.config.ts
@@ -0,0 +1,229 @@
+import type { Feed } from '../types';
+
+export const feedsConfig: Feed[] = [
+  {
+    name: "BBC World News",
+    url: "http://feeds.bbci.co.uk/news/world/rss.xml",
+    category: "News"
+  },
+  {
+    name: "NASA Breaking News",
+    url: "https://www.nasa.gov/rss/dyn/breaking_news.rss",
+    category: "Science"
+  },
+  {
+    name: "TechCrunch",
+    url: "https://techcrunch.com/feed/",
+    category: "Technology"
+  },
+  {
+    name: "Bishop Fox Technology",
+    url: "https://bishopfox.com/feeds/technology.rss",
+    category: "Security"
+  },
+  {
+    name: "Frichetten Blog",
+    url: "https://frichetten.com/blog/index.xml",
+    category: "Security"
+  },
+  {
+    name: "Hacking The Cloud",
+    url: "https://hackingthe.cloud/feed_rss_created.xml",
+    category: "Cloud Security"
+  },
+  {
+    name: "AWS Steele",
+    url: "https://awsteele.com/feed.xml",
+    category: "Cloud Security"
+  },
+  {
+    name: "PortSwigger Research",
+    url: "https://portswigger.net/research/rss",
+    category: "Security"
+  },
+  {
+    name: "SpecterOps Blog",
+    url: "https://posts.specterops.io/feed",
+    category: "Security"
+  },
+  {
+    name: "TrustedSec",
+    url: "https://trustedsec.com/feed.rss",
+    category: "Security"
+  },
+  {
+    name: "Rami Mac",
+    url: "https://ramimac.me/feed.xml",
+    category: "Security"
+  },
+  // {
+  //   name: "CloudSecList",
+  //   url: "https://cloudseclist.com/feed.xml",
+  //   category: "Cloud Security"
+  // },
+  {
+    name: "Google Project Zero",
+    url: "https://googleprojectzero.blogspot.com/feeds/posts/default",
+    category: "Security"
+  },
+  {
+    name: "Google Online Security Blog",
+    url: "https://feeds.feedburner.com/GoogleOnlineSecurityBlog",
+    category: "Security"
+  },
+  {
+    name: "Skybound",
+    url: "https://www.skybound.link/index.xml",
+    category: "Security"
+  },
+  {
+    name: "Riccardo Ancarani",
+    url: "https://riccardoancarani.github.io/feed.xml",
+    category: "Security"
+  },
+  {
+    name: "Trail of Bits Blog",
+    url: "https://blog.trailofbits.com/feed/",
+    category: "Security"
+  },
+  {
+    name: "Alex Plaskett",
+    url: "https://alexplaskett.github.io/feed.xml",
+    category: "Security"
+  },
+  {
+    name: "Dev Zzo Blarg",
+    url: "https://dev-zzo.github.io/blarg/feed.xml",
+    category: "Security"
+  },
+  {
+    name: "Ethical Chaos",
+    url: "https://ethicalchaos.dev/feed/",
+    category: "Security"
+  },
+  {
+    name: "Enigma0x3",
+    url: "https://enigma0x3.net/feed/",
+    category: "Security"
+  },
+  {
+    name: "Mathy Vanhoef",
+    url: "https://www.mathyvanhoef.com/feeds/posts/default",
+    category: "Security"
+  },
+  {
+    name: "MJG59",
+    url: "https://mjg59.dreamwidth.org/data/rss",
+    category: "Security"
+  },
+  {
+    name: "Soatok Blog",
+    url: "https://soatok.blog/feed/",
+    category: "Security"
+  },
+  {
+    name: "Elie.net",
+    url: "https://elie.net/rss.xml",
+    category: "Security"
+  },
+  {
+    name: "Dirkjanm.io",
+    url: "https://dirkjanm.io/feed.xml",
+    category: "Security"
+  },
+  {
+    name: "Hboeck Blog",
+    url: "https://blog.hboeck.de/rss.xml",
+    category: "Security"
+  },
+  {
+    name: "Beehiiv",
+    url: "https://rss.beehiiv.com/feeds/xgTKUmMmUm.xml",
+    category: "Security"
+  },
+  {
+    name: "Datadog Security Labs",
+    url: "https://securitylabs.datadoghq.com/rss/feed.xml",
+    category: "Security"
+  },
+  {
+    name: "Dark Reading",
+    url: "https://www.darkreading.com/rss.xml",
+    category: "Security"
+  },
+  {
+    name: "Wiz Research",
+    url: "https://www.wiz.io/feed/tag/research/rss.xml",
+    category: "Cloud Security"
+  },
+  {
+    name: "Hack The Box All Content",
+    url: "https://www.hackthebox.com/rss/blog/all",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box Red Teaming",
+    url: "https://www.hackthebox.com/rss/blog/red-teaming",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box Blue Teaming",
+    url: "https://www.hackthebox.com/rss/blog/blue-teaming",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box Cyber Teams",
+    url: "https://www.hackthebox.com/rss/blog/cyber-teams",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box Education",
+    url: "https://www.hackthebox.com/rss/blog/education",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box CISO Diaries",
+    url: "https://www.hackthebox.com/rss/blog/ciso-diaries",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box Events",
+    url: "https://www.hackthebox.com/rss/blog/events",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box HTB Insider",
+    url: "https://www.hackthebox.com/rss/blog/htb-insider",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box Customer Stories",
+    url: "https://www.hackthebox.com/rss/blog/customer-stories",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box Write-Ups",
+    url: "https://www.hackthebox.com/rss/blog/write-ups",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box CVE Explained",
+    url: "https://www.hackthebox.com/rss/blog/cve-explained",
+    category: "Security"
+  },
+  {
+    name: "Hack The Box News",
+    url: "https://www.hackthebox.com/rss/blog/news",
+    category: "Security"
+  },
+  {
+    name: "Career Stories",
+    url: "https://www.hackthebox.com/rss/blog/career-stories",
+    category: "Security"
+  },
+  {
+    name: "Humans of HTB",
+    url: "https://www.hackthebox.com/rss/blog/humans-of-htb",
+    category: "Security"
+  }
+];
